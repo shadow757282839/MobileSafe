9aef06f736ba12ea8277945621871f0e71a652e5
LiangDingfu <757282839@qq.com>
2017-10-24 21:16:21 +0800

    手机防盗界面完善

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -1,5 +1,7 @@
 package cn.edu.gdmec.android.mobileguard.m1home;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetupPasswordDialog;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.receiver.MyDeviceAdminReceiver;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
 
 
@@ -23,6 +26,10 @@ public class HomeActivity extends AppCompatActivity {
     private long mExitTime;
     /** 存储手机防盗密码的sp**/
     private SharedPreferences msharedPreferences;
+    /* 设备管理员*/
+    private DevicePolicyManager policyManager;
+    /* 申请管理员*/
+    private ComponentName componentName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +57,19 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+        //1.获取设备管理员
+        policyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        //2.申请权限 MyDeviceAdminReciever继承DeviceAdminReceiver
+        componentName = new ComponentName(this, MyDeviceAdminReceiver.class);
+        //3.判断，如果没有权限则申请权限
+        boolean active = policyManager.isAdminActive(componentName);
+        if (!active){
+            //如果没有管理员权限，则获取管理员权限
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,componentName);
+            intent.putExtra(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN,"获取超级管理员的权限，用于远程锁屏和清除数据");
+            startActivity(intent);
+        }
     }
     public void startActivity(Class<?> cls){
         Intent intent = new Intent(HomeActivity.this,cls);
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup4Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup4Activity.java
@@ -1,27 +1,59 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import cn.edu.gdmec.android.mobileguard.R;
-
-/**
- * Created by Z_er on 2017/10/14.
- */
-
 public class Setup4Activity extends BaseSetupActivity {
+    private TextView mStatusTV;
+    private ToggleButton mToggleButton;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_4);
-        //设置第2个小圆点的颜色;
+        //设置第4个小圆点的颜色;
         ((RadioButton) findViewById(R.id.rb_four)).setChecked(true);
+        initView();
     }
 
+    private void initView(){
+        ((RadioButton)findViewById(R.id.rb_four)).setChecked(true);
+        mStatusTV = (TextView) findViewById(R.id.tv_setup4_status);
+        mToggleButton = (ToggleButton) findViewById(R.id.togglebtn_securityfunction);
+        mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked){
+                if(isChecked){
+                    mStatusTV.setText("防盗保护已经开启");
+                }else{
+                    mStatusTV.setText("防盗保护没有开启");
+                }
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("protecting",isChecked);
+                editor.commit();
+            }
+        });
+
+        boolean protecting = sp.getBoolean("protecting",true);
+        if(protecting){
+            mStatusTV.setText("防盗保护已经开启");
+            mToggleButton.setChecked(true);
+        }else{
+            mStatusTV.setText("防盗保护没有开启");
+            mToggleButton.setChecked(false);
+        }
+    }
     @Override
     public void showNext() {
+        //跳转至 防盗保护界面
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isSetUp",true);
+        editor.commit();
         startActivityAndFinishSelf(LostFindActivity.class);
     }
 
